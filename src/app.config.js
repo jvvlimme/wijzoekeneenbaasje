ab786ef638303a65a19da33001389d152b06a0cf
jvvlimme <jan.vanvlimmeren@gmail.com>
2016-02-21 17:59:43 +0100

    added theme

--- src/app.config.js
+++ src/app.config.js
@@ -1,6 +1,7 @@
 routing.$inject = ['$urlRouterProvider', '$locationProvider'];
 
-export default function routing($urlRouterProvider, $locationProvider) {
+export default function routing($urlRouterProvider, $locationProvider, $mdThemeProvider) {
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
+  $mdThemeProvider.theme("default").primaryPalette("amber").accentPalette("red");
 }
--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import 'angular-material/angular-material.min.css';
 
 import angular from 'angular';
+import animate from 'angular-animate';
+import aria from 'angular-aria';
+import material from 'angular-material'
 import uirouter from 'angular-ui-router';
 
 import routing from './app.config';
 import home from './features/home';
 
-angular.module('app', [uirouter, home])
+angular.module('app', [uirouter, home, animate, aria, material])
   .config(routing);
